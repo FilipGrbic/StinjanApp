63b127736c1c4fc1036b530625b97681407ff5be
DEVK115\Panonit <grbicf97@gmail.com>
2021-05-12 16:53:43 +0200

    Project setup

---
+++ web/StinjanApp/src/main/java/com/panonit/StinjanApp/StinjanAppApplication.java
@@ -0,0 +1,13 @@
+package com.panonit.StinjanApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StinjanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StinjanAppApplication.class, args);
+	}
+
+}
---
+++ web/StinjanApp/src/test/java/com/panonit/StinjanApp/StinjanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panonit.StinjanApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StinjanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
