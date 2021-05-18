60f1e479223f43f5ca19b186e7d6f73f9f123d84
DEVK115\Panonit <grbicf97@gmail.com>
2021-05-18 15:32:14 +0200

    Angular components, services and navbar created

--- web/StinjanFrontApp/src/app/app-routing.module.ts
+++ web/StinjanFrontApp/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ApartmentsComponent } from './components/apartments/apartments.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HomeComponent } from './components/home/home.component';
+import { LunchComponent } from './components/lunch/lunch.component';
+import { TripsComponent } from './components/trips/trips.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'aboutus', component: AboutComponent},
+  {path: 'apartments', component: ApartmentsComponent},
+  {path: 'lunch', component: LunchComponent},
+  {path: 'travel', component: TripsComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/StinjanFrontApp/src/app/app.module.ts
+++ web/StinjanFrontApp/src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { ApartmentsComponent } from './components/apartments/apartments.component';
+import { TripsComponent } from './components/trips/trips.component';
+import { LunchComponent } from './components/lunch/lunch.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+
+import {TripsService} from './services/trips.service';
+import {LunchService} from './services/lunch.service';
+import {ApartmentsService} from './services/apartments.service';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    ApartmentsComponent,
+    TripsComponent,
+    LunchComponent,
+    GalleryComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    TripsService,
+    LunchService,
+    ApartmentsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
